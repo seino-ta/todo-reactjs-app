c82242da5a6a94ee365e8a5dc6271c6a5f6da88b
seino <36844233+seino-ta@users.noreply.github.com>
2022-11-24 17:39:10 +0900

    Initial commit

---
+++ src/components/IncompleteTodos.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+export const IncompleteTodos = (props) => {
+  const { todos, onClickComplete, onClickDelete } = props;
+  return (
+    <div className="incomplete-area">
+      <p className="title">未完了のTODO</p>
+      <ul>
+        {todos.map((todo, index) => {
+          return (
+            //mapで処理をするときはkeyを設定する
+            //関数に引数を渡したい場合はアロー関数を使う、これをやらないとonClickDeleteが起動時のタイミングでも走ってします
+
+            <div key={todo} className="list-row">
+              <li>{todo}</li>
+              <button onClick={() => onClickComplete(index)}>完了</button>
+              <button onClick={() => onClickDelete(index)}>削除</button>
+            </div>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
